3864895d6e7f81a02c85de4603d4934f31854e9f
Tanjim Hossain <tanjim@revesoft.com>
2017-12-31 20:51:00 +0600

    ScrollView methods, ImageView and Launchscreen updated

--- Edit-Image-Objective-C/Edit-Image-Objective-C/ViewController.m
+++ Edit-Image-Objective-C/Edit-Image-Objective-C/ViewController.m
@@ -10,15 +10,27 @@
 
 @interface ViewController ()
 
+@property (nonatomic, strong) UIImageView *imageView;
+
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    UIView *contentView = [UIView new];
+    UIImageView *imageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"default"]];
+    [contentView addSubview:imageView];
+    [_scrollView addSubview:contentView];
+    _imageView = imageView;
 }
 
+- (void)viewDidLayoutSubviews
+{
+    [super viewDidLayoutSubviews];
+    [self refreshImageView];
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
@@ -47,5 +59,59 @@
     tabBar.selectedItem = nil;
 }
 
+
+#pragma mark- ScrollView related Methods
+
+- (UIView *)viewForZoomingInScrollView:(UIScrollView *)scrollView
+{
+    return _imageView.superview;
+}
+
+- (void)scrollViewDidZoom:(UIScrollView *)scrollView
+{
+    CGFloat Ws = _scrollView.frame.size.width - _scrollView.contentInset.left - _scrollView.contentInset.right;
+    CGFloat Hs = _scrollView.frame.size.height - _scrollView.contentInset.top - _scrollView.contentInset.bottom;
+    CGFloat W = _imageView.superview.frame.size.width;
+    CGFloat H = _imageView.superview.frame.size.height;
+    
+    CGRect rct = _imageView.superview.frame;
+    rct.origin.x = MAX((Ws-W)/2, 0);
+    rct.origin.y = MAX((Hs-H)/2, 0);
+    _imageView.superview.frame = rct;
+}
+
+- (void)resetImageViewFrame
+{
+    CGSize size = (_imageView.image) ? _imageView.image.size : _imageView.frame.size;
+    CGFloat ratio = MIN(_scrollView.frame.size.width / size.width, _scrollView.frame.size.height / size.height);
+    CGFloat W = ratio * size.width;
+    CGFloat H = ratio * size.height;
+    _imageView.frame = CGRectMake(0, 0, W, H);
+    _imageView.superview.bounds = _imageView.bounds;
+}
+
+- (void)resetZoomScaleWithAnimate:(BOOL)animated
+{
+    CGFloat Rw = _scrollView.frame.size.width / _imageView.frame.size.width;
+    CGFloat Rh = _scrollView.frame.size.height / _imageView.frame.size.height;
+    
+    //CGFloat scale = [[UIScreen mainScreen] scale];
+    CGFloat scale = 1;
+    Rw = MAX(Rw, _imageView.image.size.width / (scale * _scrollView.frame.size.width));
+    Rh = MAX(Rh, _imageView.image.size.height / (scale * _scrollView.frame.size.height));
+    
+    _scrollView.contentSize = _imageView.frame.size;
+    _scrollView.minimumZoomScale = 1;
+    _scrollView.maximumZoomScale = MAX(MAX(Rw, Rh), 1);
+    
+    [_scrollView setZoomScale:_scrollView.minimumZoomScale animated:animated];
+    [self scrollViewDidZoom:_scrollView];
+}
+
+- (void)refreshImageView
+{
+    [self resetImageViewFrame];
+    [self resetZoomScaleWithAnimate:NO];
+}
 
 @end
